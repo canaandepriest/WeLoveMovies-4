edeab24382d8dd01f90e6ba8d7ce198a0f9811cd
canaandepriest <92954384+canaandepriest@users.noreply.github.com>
2022-07-03 17:25:39 -0600

    Add files via upload

---
+++ We-Love-Movies-main/src/movies/movies.router.js
@@ -0,0 +1,13 @@
+const router = require("express").Router();
+const controller = require("./movies.controller");
+const theatersRouter = require("../theaters/theaters.router");
+const reviewsRouter = require("../reviews/reviews.router");
+const methodNotAllowed = require("../errors/methodNotAllowed");
+
+router.use("/:movieId/reviews", controller.validateMovieId, reviewsRouter);
+router.use("/:movieId/theaters", controller.validateMovieId, theatersRouter);
+
+router.route("/").get(controller.list).all(methodNotAllowed);
+router.route("/:movieId").get(controller.read).all(methodNotAllowed);
+
+module.exports = router;
